c4d6a956ae4a17075ea10dc9200699fddfea03f5
Prashantverma211 <parshantverma21@gmail.com>
2024-12-29 16:01:16 +0530

    removing extra code

--- DB_Link/database.py
+++ DB_Link/database.py
@@ -1,10 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
-URLTODB = 'postgresql://postgres:password@localhost:5432/AssignmentKraft'
-# URLTODB = 'postgresql://AssignmentKraft_owner:w5EejPk0QbVO@ep-patient-tree-a5gl691c.us-east-2.aws.neon.tech/AssignmentKraft'
+load_dotenv()
 
+
+# URLTODB=os.getenv('URLTODB')
+
+# print(URLTODB)
+#hardcoding
+URLTODB="postgresql://postgres:password@localhost:5432/AssignmentKraft"
+
+
+if URLTODB is None:
+    raise ValueError("The environment variable 'URLTODB' is not set.")
 
 engine = create_engine(URLTODB)
 Session_local= sessionmaker(autocommit=False,autoflush=False,bind=engine)
